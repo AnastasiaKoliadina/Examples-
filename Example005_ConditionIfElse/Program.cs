32bfd669827e61998926dbc36979a1785251233d
Anastasia <anastasia7066084@mail.ru>
2022-11-29 19:57:04 +0300

    Example006_Base

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.write ("Введите имя пользователя:  ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.write("Привет, ");
+    Console.WriteLine(username);
+}
